7a8cf8cbe9bf3d4407833eda1271493826e33ba1
And_ZJ <1049211733@qq.com>
2019-10-30 16:48:27 +0800

    add iptables rules in loadModules.sh file

--- conntrak_opc_da/loadModules.sh
+++ conntrak_opc_da/loadModules.sh
@@ -1,8 +1,10 @@
 #! /bin/bash -x
 
 # should run this file with permissions
+iptables -D PREROUTING -t raw -p tcp --dport 135 -j CT --helper opc
 modprobe nf_conntrack
 #rmmod my_nf_nat_opc
 rmmod my_nf_conntrack_opc # remove it if existed
 insmod my_nf_conntrack_opc.ko # insmod a new module
 #insmod my_nf_nat_opc.ko
+iptables -A PREROUTING -t raw -p tcp --dport 135 -j CT --helper opc
